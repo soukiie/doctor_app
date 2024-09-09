4b1c020fdb6a9bdf819c682a0991219c5a32c798
Sayed Saad <elsayed.saad2014@feps.edu.eg>
2024-09-09 23:07:08 +0300

    *add all folders stucture to project * added images & svgs folders for assets *added flutter_svg & flutter_screenutil *added navigation extension *add routing folder that contain routes *added empty login_screen *finish onboarding_screen

---
+++ lib/doc_app.dart
@@ -0,0 +1,28 @@
+import 'package:doc_app/core/routing/app_router.dart';
+import 'package:doc_app/core/routing/routes.dart';
+import 'package:doc_app/core/theming/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class DocApp extends StatelessWidget {
+  final AppRouter appRouter;
+  const DocApp({super.key, required this.appRouter});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'DocDoc',
+        theme: ThemeData(
+          primaryColor: ColorsManager.mainBlue,
+          scaffoldBackgroundColor: Colors.white,
+        ),
+        initialRoute: Routes.onBoardingScreen,
+        onGenerateRoute: appRouter.generateRoute,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,9 @@
+import 'package:doc_app/core/routing/app_router.dart';
+import 'package:doc_app/doc_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Doctor App'),
-        ),
-      ),
-    );
-  }
+  runApp(DocApp(
+    appRouter: AppRouter(),
+  ));
 }

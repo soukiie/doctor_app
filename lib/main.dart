44461a798f30776a5d7e18d6dc7534dc367f16f1
Sayed Saad <elsayed.saad2014@feps.edu.eg>
2024-09-10 04:12:33 +0300

    *added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,26 +3,7 @@ import 'package:doc_app/doc_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-<<<<<<< HEAD
   runApp(DocApp(
     appRouter: AppRouter(),
   ));
-=======
-  runApp(const DocApp());
-}
-
-class DocApp extends StatelessWidget {
-  const DocApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Doctor App'),
-        ),
-      ),
-    );
-  }
->>>>>>> origin/development
 }
